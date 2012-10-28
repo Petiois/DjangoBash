c6d00e8ef048ba675f1d8b59d1fb2227b8104cff
François <ptitois@gmail.com>
2012-10-28 23:13:11 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+apt-get update
+apt-get install git
+apt-get install python
+
+wget https://www.djangoproject.com/download/1.4.2/tarball/
+tar xzvf Django-1.4.2.tar.gz
+python Django-1.4.2/setup.py install
+git clone https://github.com/Petiois/videoShare.git
+
+python manage.py syncdb
+python manage.py runserver
+
+wget http://127.0.0.1/
+
+
+exit 0
