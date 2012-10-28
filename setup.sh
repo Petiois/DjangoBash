388ce11a81f2680c0145d2d7760eba963750a3a5
François <ptitois@gmail.com>
2012-10-28 23:26:08 +0100

    add db

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ apt-get install git
 apt-get install python
 
 wget https://www.djangoproject.com/download/1.4.2/tarball/
+mv index.html Django-1.4.2.tar.gz
 tar xzvf Django-1.4.2.tar.gz
 python Django-1.4.2/setup.py install
 git clone https://github.com/Petiois/videoShare.git
